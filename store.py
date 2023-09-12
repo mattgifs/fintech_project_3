104b2284728815e06d1406e405a08061bef0dc72
Matt Gifford <giffordmatt00@gmail.com>
2023-09-11 22:17:12 -0400

    chgs to store.py, some changes to contract

--- store.py
+++ store.py
@@ -8,61 +8,59 @@ import streamlit as st
 load_dotenv()
 
 # Define and connect a new Web3 provider
-#w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
+w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 
 ################################################################################
 # The Load_Contract Function
 ################################################################################
 
-'''
+
 @st.cache_resource()
 def load_contract():
 
     # Load the contract ABI
-    with open(Path('./contracts/compiled/artwork_abi.json')) as f:
-        artwork_abi = json.load(f)
+    with open(Path('./contracts/compiled/mem_token_abi.json')) as f:
+        mem_token_abi = json.load(f)
 
     contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
 
     # Load the contract
     contract = w3.eth.contract(
         address=contract_address,
-        abi=artwork_abi
+        abi=mem_token_abi
     )
 
     return contract
 
 contract = load_contract()
-'''
+
 
 ################################################################################
 # Register New Artwork
 ################################################################################
 st.title("Purchase Your Wine Membership")
-#accounts = w3.eth.accounts
+accounts = w3.eth.accounts
 regions = ['Russian River Valley','Napa', 'Champagne', 'Somewhere']
 
 # Use a Streamlit component to get the address of the artwork owner from the user
-#address = st.selectbox("Select Artwork Owner", options=accounts)
-address = st.selectbox("Choose your region:", options=regions)
+address = st.selectbox("Select Artwork Owner", options=accounts)
+region = st.selectbox("Choose your region:", options=regions)
 
-# Use a Streamlit component to get the artwork's URI
-#artwork_uri = st.text_input("The URI to the artwork")
 
+traitIndex = st.selectbox("Trait Index:", options=[0,1])
 
 if st.button("Purchase Membership"):
-    st.write(f"Purchase Complete!")
 
-    '''
     # Use the contract to send a transaction to the registerArtwork function
-    tx_hash = contract.functions.registerArtwork(
+    tx_hash = contract.functions.mintMembership(
         address,
-        artwork_uri
+        traitIndex
     ).transact({'from': address, 'gas': 1000000})
     receipt = w3.eth.waitForTransactionReceipt(tx_hash)
     st.write("Transaction receipt mined:")
     st.write(dict(receipt))
-'''
+    st.write(f"Purchase Complete!")
+
 st.markdown("---")
 
 
@@ -84,7 +82,7 @@ st.text_input("ZIP Code")
 
 
 
-'''
+
 selected_address = st.selectbox("Select Account", options=accounts)
 
 tokens = contract.functions.balanceOf(selected_address).call()
@@ -96,18 +94,18 @@ st.markdown("## Check  Ownership and Display Token")
 total_token_supply = contract.functions.totalSupply().call()
 
 token_id = st.selectbox("Artwork Tokens", list(range(total_token_supply)))
-'''
+
 if st.button("Display"):
     st.write(f"Thanks for submitting your information and joining our club!")
-    '''
-    # Get the art token owner
-    owner = contract.functions.ownerOf(token_id).call()
     
-    st.write(f"The token is registered to {owner}")
-
-    # Get the art token's URI
-    token_uri = contract.functions.tokenURI(token_id).call()
-
-    st.write(f"The tokenURI is {token_uri}")
-    st.image(token_uri)
-    '''
+#    # Get the art token owner
+#    owner = contract.functions.ownerOf(token_id).call()
+#    
+#    st.write(f"The token is registered to {owner}")
+#
+#    # Get the art token's URI
+#    token_uri = contract.functions.tokenURI(token_id).call()
+#
+#    st.write(f"The tokenURI is {token_uri}")
+#    st.image(token_uri)
+    
