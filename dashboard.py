484ca4a25fab97fed56fb0fffd6c53435e927a3f
Matt Gifford <giffordmatt00@gmail.com>
2023-09-18 21:22:58 -0400

    added some comments on demo_purchases, added titles to dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -10,9 +10,14 @@ import pandas as pd
 from PIL import Image
 load_dotenv()
 
-
+#Title the dashboard
 st.title("Wine Membership Dashboard")
 st.markdown('---')
+
+
+################################################################
+# Query Customer Datatable, add to dataframe
+################################################################
 
 # Create postgres engine
 engine = create_engine("postgresql://postgres:postgres@localhost:5432/fintech_project_3")
@@ -26,6 +31,10 @@ SELECT * FROM customer_data
 df = pd.read_sql(query, engine)
 df.set_index('tx_id',inplace=True)
 
+
+################################################################
+# Custom dataframes & Plots
+################################################################
 
 col1,col2 = st.columns(2)
 
@@ -43,8 +52,10 @@ grouped_df['Region'] = grouped_df['traitindex'].map(regions)
 grouped_df.set_index('Region',inplace=True)
 grouped_df.sort_values('traitindex',ascending=True)
 
-#Plot 
+#Plot Count of Memberships sold by region
+col1.markdown('#### Memberships Sold by Region')
 col1.bar_chart(grouped_df,y='count', use_container_width=True)
+
 
 
 # Cumsum amount raised from sales
@@ -53,10 +64,14 @@ df["ETH Raised"] = df['eth_price'].cumsum()
 # Cumsum gas fees incurred
 df["Cumulative Gas"] = df['gas_cost'].cumsum()
 
-#Plot 
+#Plot Cumulative Gas Fees Incurred & Cumulative ETH Raised 
+col2.markdown('#### Cumulative Gas Fees Incurred')
 col2.line_chart(df['Cumulative Gas'])
+
+st.markdown('#### Cumulative ETH Raised')
 st.line_chart(df['ETH Raised'])
 
 
-
+# Display full customer_data dataframe
+st.markdown('#### Transactions')
 st.dataframe(data=df,use_container_width=False)
